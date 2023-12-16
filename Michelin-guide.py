5d23ab1a327883605eb8e6f921ce1efb8cc15460
Nicole20002777 <153608084+Nicole20002777@users.noreply.github.com>
2023-12-17 01:41:05 +0800

    TripAdvisor- PRU restaurant

--- Michelin-guide.py
+++ Michelin-guide.py
@@ -1,41 +1,121 @@
+# 載入 selenium 相關模組
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException  
+from selenium.webdriver.common.keys import Keys
 import time
 
-#設定Chrome Driver 的執行檔路徑
-options=Options()
-options.chrome_executable_path="C:\python\chromedriver.exe"
-#建立 driver物件實體，用程式操作瀏覽器運作
-driver=webdriver.Chrome(options=options)
+# 設定 Chrome Driver 的執行檔路徑
+options = Options()
+options.chrome_executable_path = "C:\python\chromedriver.exe"
 
-# 連線到 Michelin Guide Sustainable Gastronomy 頁面
-driver.get("https://guide.michelin.com/en/restaurants/sustainable_gastronomy")
+# 建立 driver 物件實體，用程式操作瀏覽器運作
+options.add_argument('headless')
+options.add_argument("--disable-web-security")
+options.add_argument("--disable-site-isolation-trials")
+driver = webdriver.Chrome(options=options)
 
-# 使用 class name 定位所有符合條件的元素
-restaurant_name_elements = driver.find_elements(By.CLASS_NAME, "card__menu-content--title")
+# 連線到 TripAdvisor
+driver.get("https://www.tripadvisor.com/Restaurant_Review-g1224250-d11913607-Reviews-PRU_Restaurant-Choeng_Thale_Thalang_District_Phuket.html")
 
-for restaurant_name_element in restaurant_name_elements:
-    restaurant_name = restaurant_name_element.text
-    print(restaurant_name)
+# 使用 WebDriverWait 等待 "more" 按鈕載入，增加等待時間
+wait = WebDriverWait(driver, 30)  
 
-# 確保瀏覽器有足夠的時間完成操作
-time.sleep(10)
+# 等待評論元素載入
+wait = WebDriverWait(driver, 60)
+try:
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "noQuotes"))) 
+    
+    # 載入 more 按鈕
+    time.sleep(5)
+    
+    # 尋找 more 按鈕
+    more_buttons = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'span.taLnk.ulBlueLinks')))
 
-driver.quit()
+    # 點擊所有 more 按鈕
+    for button in more_buttons:
+        try:
+            button.click()
+        except:
+            pass
+
+except TimeoutException as ex:
+    print("TimeoutException:", ex)
+
+# 模擬按下 PageDown 鍵，滾動頁面
+driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
+
+# 使用 WebDriverWait 等待 "more" 按鈕載入
+wait = WebDriverWait(driver, 10)
+more_buttons = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.taLnk.ulBlueLinks')))
+
+# 獲取留言的星等
+star_rating_tags = driver.find_elements(By.CSS_SELECTOR, 'span.ui_bubble_rating')
+star_ratings = []
+
+for star_rating_tag in star_rating_tags:
+    class_value = star_rating_tag.get_attribute("class")
+    # 從 class_value 中提取星等數字
+    star_rating = int(class_value.split("_")[-1])
+    star_ratings.append(star_rating)
+
+review_elements = driver.find_elements(By.CSS_SELECTOR, 'div.prw_rup.prw_reviews_text_summary_hsx')
+
+# 取得留言的標題、內容和時間、評論者姓名、所在地
+title_tags = driver.find_elements(By.CLASS_NAME, "noQuotes")
+content_tags = driver.find_elements(By.CLASS_NAME, "partial_entry")
+date_tags = driver.find_elements(By.CSS_SELECTOR, 'span.ratingDate')
+name_tags = driver.find_elements(By.CLASS_NAME, 'info_text')
+location_tags = driver.find_elements(By.CLASS_NAME, 'userLoc')
+
+# 建立用來儲存資料的列表
+titles = []
+contents = []
+dates = []
+names = []
+locations = []
+ratings = []
 
 
+# 迭代所有星等元素的標籤
+for star_rating_tag in star_rating_tags:
+    class_value = star_rating_tag.get_attribute("class")
+    # 從 class_value 中提取星等數字
+    star_rating = int(class_value.split("_")[-1]) // 10  # 除以10取整數部分，例如：40 → 4, 50 → 5
+    ratings.append(star_rating)  # 將星等數字轉換為浮點數並除以10，然後存入列表
+
+# 逐一處理每個評論
+for i in range(len(title_tags)):
+    title = title_tags[i].text
+    content = content_tags[i].text
+    date = date_tags[i].text
+    name = name_tags[i].text
+    rating = ratings[i]
 
 
+    # 將資料附加到對應的列表中
+    titles.append(title)
+    contents.append(content)
+    dates.append(date)
+    names.append(name)
+    ratings.append(rating)
 
-# tags=driver.find_elements(By.CLASS_NAME,"col order-2 order-lg-1 mb-lg-0 mt-1 mt-lg-0") #抓很多個標籤要加s(BY搜尋 class 屬性是 title 的所有標籤)
+    # 印出評論標題、內容和時間（可選步驟，用於檢查）
+    print(f"Title: {title}")
+    print(f"Content: {content}")
+    print(f"Time: {date}")
+    print(f"Star Rating: {rating}")
+    print(f"Name: {name}")
+    print("-" * 50)
 
-# for tag in tags:
-#     print(tag.text) #印出來後為一個列表，包含所有標籤的資訊，逐一取得列表內部的每一個標籤，並取得標籤內部的文字
+    # 逐一處理每個評論，只處理奇數索引位置的元素
+for i in range(1, len(review_elements), 2):
+    content = review_elements[i].find_element(By.CLASS_NAME, 'partial_entry').text.strip()
+    print(f"Customer Review {i // 2 + 1}: {content}")
 
-# #取得上一頁的文章標題(概念：根據超連結的內文來搜尋取得標籤命名為(link)，再模擬使用者的點擊)
-# link=driver.find_element(By.LINK_TEXT,"‹ 上頁") #只要蒐集"上頁"，只有一個標籤
-# link.click()  # 模擬使用者點擊(上一頁)連結標籤
-# tags=driver.find_elements(By.CLASS_NAME,"title") #抓很多個標籤要加s(BY搜尋 class 屬性是 title 的所有標籤)
-# for tag in tags:
-#  print(tag.text) #印出來後為一個列表，包含所有標籤的資訊，逐一取得列表內部的每一個標籤，並取得標籤內部的文字
+
+# 關閉瀏覽器
+driver.close()
